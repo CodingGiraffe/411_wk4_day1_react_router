4613d57ec0d28a203eb0b3c9359bed8b1536c2da
CodingGiraffe <adamthomas03@gmail.com>
2020-08-26 10:09:27 -0500

    finished homework

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Container, Divider, Switch } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 class About extends Component {
     state = {
